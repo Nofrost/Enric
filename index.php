b2d67079374f82742d9f63cf0423cbd8234558c2
Nofrost <danmarc.rb@gmail.com>
2014-10-13 16:32:07 +0200

    Primera versió

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<!-- Configurations & CSS -->
+		<link type="text/css" rel="stylesheet" href="Enric.css"/>
+        <link type="text/css" rel="stylesheet" href="scrollbar_h.css"/>
+        <link href='http://fonts.googleapis.com/css?family=Cinzel' rel='stylesheet' type='text/css'>
+        
+		<title>Enric_Mu&#241oz</title>
+	</head>
+
+	<body>
+            <div id="logo">
+                <a href="index.php"><img src="Imatges\Logo.png"/></a>
+            </div>
+        <header>
+            <div class="intro">
+                <p><span id="titol">ENRIC MU&#209OZ</span><br>
+                <span id="sub1">HOT DESIGN / EMOTIONAL COMMUNICATION</span><br>
+                <span id="sub2">by KILOGRAFIK</span></p>
+            </div>
+
+            <div>
+                <p id="menuSuperior"><span>EDITORIAL DESIGN</span> / <span>IDENTITY</span> / <span>PROMOTION & PUB</span> /<br>
+                    <span>EVENTS & SCREEN</span> / <span>ILLUSTRATION & PHOTO WORKS</span> /<br>
+                    <span>INTRO & VIEW MIX</span> /
+                </p>
+            </div>
+        </header>
+        
+        <div style="height: 400px">
+            <div id="wn" style="height: 340px"> <!-- scroll area div -->
+                <div id="lyr1"> <!-- layer in scroll area (content div) -->
+                    <table id="taula_ordre" border="0" cellpadding="0" cellspacing="0">
+                        <tr class="images">
+                            <td><img src="Imatges/Imatge_1.png" alt="" /></td>
+                            <td><img src="Imatges/Space.png" alt="" /></td>
+                            <td><img src="Imatges/Imatge_2.png" alt="" /></td>
+                            <td><img src="Imatges/Space.png" alt="" /></td>
+                            <td><img src="Imatges/Exemple/Adults_03.jpg" alt="" /></td>
+                            <td><img src="Imatges/Space.png" alt="" /></td>
+                            <td><img src="Imatges/Exemple/Adults_04.jpg" alt="" /></td>
+                            <td><img src="Imatges/Space.png" alt="" /></td>
+                            <td><img src="Imatges/Exemple/Adults_05.jpg" alt="" /></td>
+                            <td><img src="Imatges/Space.png" alt="" /></td>
+                            <td><img src="Imatges/Exemple/Adults_01.jpg" alt="" /></td>
+                            <td><img src="Imatges/Space.png" alt="" /></td>
+                            <td><img src="Imatges/Exemple/Adults_02.jpg" alt="" /></td>
+                            </tr>
+                    </table>
+                </div>
+            </div><!-- end wn div -->
+        </div>
+        <div id="scrollbar" style="visibility: visible;">
+            <div id="wn_h_left" class="left"></div>
+                <div id="wn_h_track" class="track" tabindex="0">
+                    
+                </div>
+                <div id="wn_h_right" class="right">
+                </div>
+            </div>
+        <footer>
+            <img src="Imatges\Footer.png"/>
+        </footer>
+        
+	</body>
+</html>
