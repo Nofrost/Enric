3fad7057e1c1b1951b1bb5477be9fb1f35c518cb
Leninux <redrudeboy@gmail.com>
2014-10-15 20:30:22 +0200

    Primer dia al local

--- index.php
+++ index.php
@@ -5,66 +5,78 @@
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 	<head>
 		<!-- Configurations & CSS -->
+		<link href='http://fonts.googleapis.com/css?family=Cinzel' rel='stylesheet' type='text/css'>
+		<link type="text/css" rel="stylesheet" href="scrollbar_h.css"/>
 		<link type="text/css" rel="stylesheet" href="Enric.css"/>
-        <link type="text/css" rel="stylesheet" href="scrollbar_h.css"/>
-        <link href='http://fonts.googleapis.com/css?family=Cinzel' rel='stylesheet' type='text/css'>
-        
+
 		<title>Enric_Mu&#241oz</title>
 	</head>
 
 	<body>
-            <div id="logo">
-                <a href="index.php"><img src="Imatges\Logo.png"/></a>
-            </div>
-        <header>
-            <div class="intro">
-                <p><span id="titol">ENRIC MU&#209OZ</span><br>
-                <span id="sub1">HOT DESIGN / EMOTIONAL COMMUNICATION</span><br>
-                <span id="sub2">by KILOGRAFIK</span></p>
-            </div>
+		<!--<div id="guia_layout"></div>-->
 
-            <div>
-                <p id="menuSuperior"><span>EDITORIAL DESIGN</span> / <span>IDENTITY</span> / <span>PROMOTION & PUB</span> /<br>
-                    <span>EVENTS & SCREEN</span> / <span>ILLUSTRATION & PHOTO WORKS</span> /<br>
-                    <span>INTRO & VIEW MIX</span> /
-                </p>
-            </div>
-        </header>
-        
-        <div style="height: 400px">
-            <div id="wn" style="height: 340px"> <!-- scroll area div -->
-                <div id="lyr1"> <!-- layer in scroll area (content div) -->
-                    <table id="taula_ordre" border="0" cellpadding="0" cellspacing="0">
-                        <tr class="images">
-                            <td><img src="Imatges/Imatge_1.png" alt="" /></td>
-                            <td><img src="Imatges/Space.png" alt="" /></td>
-                            <td><img src="Imatges/Imatge_2.png" alt="" /></td>
-                            <td><img src="Imatges/Space.png" alt="" /></td>
-                            <td><img src="Imatges/Exemple/Adults_03.jpg" alt="" /></td>
-                            <td><img src="Imatges/Space.png" alt="" /></td>
-                            <td><img src="Imatges/Exemple/Adults_04.jpg" alt="" /></td>
-                            <td><img src="Imatges/Space.png" alt="" /></td>
-                            <td><img src="Imatges/Exemple/Adults_05.jpg" alt="" /></td>
-                            <td><img src="Imatges/Space.png" alt="" /></td>
-                            <td><img src="Imatges/Exemple/Adults_01.jpg" alt="" /></td>
-                            <td><img src="Imatges/Space.png" alt="" /></td>
-                            <td><img src="Imatges/Exemple/Adults_02.jpg" alt="" /></td>
-                            </tr>
-                    </table>
-                </div>
-            </div><!-- end wn div -->
-        </div>
-        <div id="scrollbar" style="visibility: visible;">
-            <div id="wn_h_left" class="left"></div>
-                <div id="wn_h_track" class="track" tabindex="0">
-                    
-                </div>
-                <div id="wn_h_right" class="right">
-                </div>
-            </div>
-        <footer>
-            <img src="Imatges\Footer.png"/>
-        </footer>
-        
+		<header>
+			<div id="header_wrapper">
+				<div id="logo">
+					<a href="index.php"><img src="Imatges/Logo.png" /></a>
+				</div>
+
+				<div id="menu">
+					<p id="menuLeft">
+						<span id="titol">ENRIC MU&#209OZ</span><br />
+						<span id="sub1">HOT DESIGN / EMOTIONAL COMMUNICATION</span><br />
+						<span id="sub2">by KILOGRAFIK</span>
+					</p>
+
+					<p id="menuRight">
+						<span>EDITORIAL DESIGN</span> / <span>IDENTITY</span> / <span>PROMOTION & PUB</span> /<br />
+						<span>EVENTS & SCREEN</span> / <span>ILLUSTRATION & PHOTO WORKS</span> /<br />
+						<span>INTRO & VIEW MIX</span> /
+					</p>
+					<div style="clear: both;"></div>
+				</div>
+			</div>
+		</header>
+
+		<div id="main">
+			<div id="wn" style="height: 340px"> <!-- scroll area div -->
+				<div id="lyr1"> <!-- layer in scroll area (content div) -->
+					<table id="taula_ordre" border="0" cellpadding="0" cellspacing="0">
+						<tr class="images">
+							<td><img src="Imatges/Imatge_1.png" alt="" /></td>
+							<td><img src="Imatges/Space.png" alt="" /></td>
+							<td><img src="Imatges/Imatge_2.png" alt="" /></td>
+							<td><img src="Imatges/Space.png" alt="" /></td>
+							<td><img src="Imatges/Exemple/Adults_03.jpg" alt="" /></td>
+							<td><img src="Imatges/Space.png" alt="" /></td>
+							<td><img src="Imatges/Exemple/Adults_04.jpg" alt="" /></td>
+							<td><img src="Imatges/Space.png" alt="" /></td>
+							<td><img src="Imatges/Exemple/Adults_05.jpg" alt="" /></td>
+							<td><img src="Imatges/Space.png" alt="" /></td>
+							<td><img src="Imatges/Exemple/Adults_01.jpg" alt="" /></td>
+							<td><img src="Imatges/Space.png" alt="" /></td>
+							<td><img src="Imatges/Exemple/Adults_02.jpg" alt="" /></td>
+						</tr>
+					</table>
+				</div>
+			</div><!-- end wn div -->
+
+			<div id="scrollbar" style="visibility: visible;">
+				<div id="wn_h_left" class="left"></div>
+				<div id="wn_h_track" class="track" tabindex="0"></div>
+				<div id="wn_h_right" class="right"></div>
+			</div>
+			<div id="scrollbar_b"></div>
+		</div>
+
+		<footer>
+			<div id="footer_icons">
+				<div id="footer_about"></div>
+				<div id="footer_mail"></div>
+				<div id="footer_linkedin"></div>
+				<div id="footer_copyright"></div>
+			</div>
+		</footer>
+
 	</body>
 </html>
