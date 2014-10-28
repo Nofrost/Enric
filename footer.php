907f2edd3712c9d21280d2142cba50325b0af36f
Nofrost <danmarc.rb@gmail.com>
2014-10-28 13:31:02 +0100

    faltan ombres i scrollbar

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
 <?php if(!isset($page)) { header('Location: index.php');die('Redirect a index'); } ?>
 <footer>
 	<div id="footer_icons">
-		<div id="footer_about"><img src="Imatges/Botons01.svg"></div>
+		<div id="footer_about"></div>
 		<div id="footer_mail">
-            <a href="mailto:emunoz@grafic.info" id="btn_mail"><img src="Imatges/Botons03.svg"></a>
+            <a href="mailto:emunoz@grafic.info" id="btn_mail"></a>
         </div>
 		<div id="footer_linkedin">
-            <a href="https://www.linkedin.com/"><img src="Imatges/Botons07.svg"/></a>
+            <a href="https://www.linkedin.com/pub/enric-mu%C3%B1oz/10/450/ba5/en" target="_blank"></a>
         </div>
-		<div id="footer_copyright"><img src="Imatges/Botons09.svg"></div>
+		<div id="footer_copyright"></div>
         <div id="about">ABOUT & INFO</div>
 
 	</div>
